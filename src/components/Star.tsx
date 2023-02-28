a6f19c08ff9646aac75232c364899f0b9619007a
PiscesSix <triedmlaunchpad@gmail.com>
2023-02-28 17:37:47 +0700

    settingStars

--- src/components/Star.tsx
+++ src/components/Star.tsx
@@ -5,11 +5,13 @@ const Star = () => {
         let count = 200;
         const app = document.querySelector('.App')
         let i = 0;
+
         while (i < count){
             let star = document.createElement("i");
             let x = Math.floor(Math.random() * window.innerWidth);
             let y = Math.floor(Math.random() * window.innerHeight);
             let duration = Math.random() * 10;
+
             let size = Math.random() * 2;
 
             star.style.left = x + 'px';
@@ -17,8 +19,12 @@ const Star = () => {
             star.style.width = 1 + size + 'px';
             star.style.height = 1 + size + 'px';
 
+            if (duration < 3){
+                duration = 0;
+            }
+
             star.style.animationDuration = duration + 's';
-            star.style.animationDelay = duration + 's';
+            star.style.animationDelay = '1s';
             
             app?.appendChild(star);
             i++;
